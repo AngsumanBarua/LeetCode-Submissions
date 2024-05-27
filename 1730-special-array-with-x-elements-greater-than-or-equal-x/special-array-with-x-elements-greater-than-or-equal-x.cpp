ac49ec2bdc9fb9978017b4346a1e98d8f4405a2f
AngsumanBarua <92010061+AngsumanBarua@users.noreply.github.com>
2024-05-27 22:01:17 +0600

    Time: 0 ms (100.00%) | Memory: 10.2 MB (19.80%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     int specialArray(vector<int>& nums) {
         int n=nums.size();
-        int val[1002];
-        int mx=0;
+        int val[n+1];
         memset(val,0,sizeof(val));
         for(int i=0;i<n;i++){
-            val[nums[i]]++;
-            mx=max(mx,nums[i]);
+            val[min(n,nums[i])]++;
         }
         int cur=0;
-        for(int i=mx;i>=1;i--){
+        for(int i=n;i>=1;i--){
             cur+=val[i];
             if(cur==i){
                 return i;
