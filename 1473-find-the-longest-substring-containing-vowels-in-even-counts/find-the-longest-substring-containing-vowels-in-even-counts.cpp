8b51421e75ec5218af63587ddf7b3b4e93936032
AngsumanBarua <92010061+AngsumanBarua@users.noreply.github.com>
2024-09-16 20:03:40 +0600

    Time: 44 ms (69.50%) | Memory: 17.6 MB (75.46%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -1,29 +1,42 @@
 class Solution {
 public:
     int findTheLongestSubstring(string s) {
-        int prefixXOR = 0;
-        // Store the masks of all letters in an array.
-        int characterMap[26] = {0};
-        characterMap['a' - 'a'] = 1;
-        characterMap['e' - 'a'] = 2;
-        characterMap['i' - 'a'] = 4;
-        characterMap['o' - 'a'] = 8;
-        characterMap['u' - 'a'] = 16;
-        // Initialize mp to store the previous index with this prefixXOR value.
-        vector<int> mp(32, -1);
-        int longestSubstring = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            // If the current character is a vowel, find it's prefix XOR and add
-            // it in the map.
-            prefixXOR ^= characterMap[s[i] - 'a'];
-            if (mp[prefixXOR] == -1 and prefixXOR != 0) mp[prefixXOR] = i;
-
-            // If the value of prefixXOR exists in the map, find the longest
-            // subarray.
-            longestSubstring = max(longestSubstring, i - mp[prefixXOR]);
+        int l=s.length();
+        int xr=0;
+        vector <int> mp(32,-1);
+        int ans=0;
+        for(int i=0;i<l;i++){
+            int val;
+            if(s[i]=='a'){
+                val=1;
+            }
+            else if(s[i]=='e'){
+                val=2;
+            }
+            else if(s[i]=='i'){
+                val=4;
+            }
+            else if(s[i]=='o'){
+                val=8;
+            }
+            else if(s[i]=='u'){
+                val=16;
+            }
+            else{
+                val=0;
+            }
+            xr^=val;
+            if(xr==0){
+                ans=i+1;
+            }
+            else if(mp[xr]==-1){
+                mp[xr]=i;
+            }
+            else{
+                ans=max(ans,i-mp[xr]);
+            }
+            //cout << xr << " " << ans << endl;
         }
-
-        return longestSubstring;
+        return ans;
     }
 };
