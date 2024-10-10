f87f414da0fa31081acbad98086cd4f47c30e433
AngsumanBarua <92010061+AngsumanBarua@users.noreply.github.com>
2024-10-10 21:58:55 +0600

    Time: 802 ms (5.39%) | Memory: 380.9 MB (5.01%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/maximum-width-ramp.cpp
@@ -3,19 +3,26 @@ public:
     int maxWidthRamp(vector<int>& nums) {
         int n=nums.size();
         int ans=0;
-        vector <int> mx(n);
-        mx[n-1]=nums[n-1];
+        stack <pair<int,int>> st;
+        st.push(make_pair(nums[n-1],n-1));
         for(int i=n-2;i>=0;i--){
-            mx[i]=max(mx[i+1],nums[i]);
+            if(st.top().first<nums[i]){
+                st.push(make_pair(nums[i],i));
+            }
         }
-        for(int i=0;i<n-1;i++){
-            if(nums[i]<=mx[i+1]){
-                for(int j=n-1;j>i;j--){
-                    if(nums[i]<=nums[j]){
-                        ans=max(ans,j-i);
-                        break;
-                    }
-                }
+        for(int i=0;i<n;i++){
+            stack <pair<int,int>> temp;
+            while(!st.empty() && st.top().first>=nums[i]){
+                ans=max(ans,st.top().second-i);
+                temp.push(make_pair(st.top().first,st.top().second));
+                st.pop();
+            }
+            while(!temp.empty()){
+                st.push(make_pair(temp.top().first,temp.top().second));
+                temp.pop();
+            }
+            while(!st.empty() && st.top().second<=i){
+                st.pop();
             }
         }
         return ans;
